456a4abded0a3a89076c3f0636bf569debf3261d
casaxmore <casaxmore@gmail.com>
2023-02-26 20:03:52 +0100

    Server

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+// Crear un nuevo usuario con su array de middleware
+router.post('/new', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').isLength({min:6}),
+    validarCampos
+] ,crearUsuario);
+
+// Login de usuario con su array de middleware
+router.post('/', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').isLength({min:6}),
+    validarCampos
+] ,loginUsuario);  
+
+// Validar y revalidar token
+router.get('/renew', validarJWT,revalidarToken);
+
+
+
+
+// Exportar
+module.exports = router;
